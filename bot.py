fc39798f09f1edc97e327608fe034e12db1c62be
Sahib75 <singhsahib7582@gmail.com>
2025-06-13 13:53:12 +0530

    fix: removed redundant decode

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ if not IG_COOKIE:
 print("🔍 Escaped Preview:", IG_COOKIE[:100])
 
 # Write cookie.txt from env
-cookie_content = codecs.decode(IG_COOKIE, "unicode_escape")
+cookie_content = IG_COOKIE
 
 print("🔓 Decoded Preview:", cookie_content[:100])
 
